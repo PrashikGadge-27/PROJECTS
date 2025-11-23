246214b977fec72d56cdbf59e62ac082bddc37df
PRASHIK GADGE <126453002+rajurastogi02@users.noreply.github.com>
2025-11-23 15:47:45 +0530

    Add files via upload

---
+++ ECommerce/eCommerce_Project-master/eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using eCommerceStarterCode.Configuration;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceStarterCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RolesConfiguration());
+        }
+
+    }
+}
